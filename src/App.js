e4c0deb7993f01e986b7f03df2a7c1ca886d2c6f
Irina Vasilescu <irina>
2021-04-02 00:25:35 +0300

    moved Map in its own file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import './App.css';
-import 'leaflet/dist/leaflet.css';
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import Map from './Map';
 
 function App() {
-    
-    const L = window.L;
-
     return (
         <div>
-            <MapContainer style={{height: '700px'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+            <Map/>
         </div>
     );
 }
