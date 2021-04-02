0e49fb13fd9cefb517104b5040869ba1370eb8c2
Irina Vasilescu <irina>
2021-04-02 17:40:40 +0300

    created Spots component

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,18 +1,13 @@
 import React from 'react';
-import clsx from 'clsx';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import CssBaseline from '@material-ui/core/CssBaseline';
-import IconButton from '@material-ui/core/IconButton';
 
 // Icons
 import ExploreIcon from '@material-ui/icons/Explore';
 import FavoriteIcon from '@material-ui/icons/Favorite';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
-import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
 
 // Styles
 import './Dashboard.css';
@@ -21,55 +16,9 @@ import './Dashboard.css';
 import Map from '../Map/Map';
 import Spots from '../Spots/Spots';
 
-const drawerWidth = '50vw';
-
 const useStyles = makeStyles((theme) => ({
     root: {
         display: 'flex',
-    },
-    appBar: {
-        zIndex: theme.zIndex.drawer + 1,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-    },
-    appBarShift: {
-        marginLeft: drawerWidth,
-        width: `calc(100% - ${drawerWidth}px)`,
-        transition: theme.transitions.create(['width', 'margin'], {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    menuButton: {
-        marginRight: 36,
-    },
-    hide: {
-        display: 'none',
-    },
-    drawer: {
-        width: drawerWidth,
-        flexShrink: 0,
-        whiteSpace: 'nowrap',
-    },
-    drawerOpen: {
-        width: drawerWidth,
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.enteringScreen,
-        }),
-    },
-    drawerClose: {
-        transition: theme.transitions.create('width', {
-            easing: theme.transitions.easing.sharp,
-            duration: theme.transitions.duration.leavingScreen,
-        }),
-        overflowX: 'hidden',
-        width: theme.spacing(7) + 1,
-        [theme.breakpoints.up('sm')]: {
-            width: theme.spacing(9) + 1,
-        },
     },
     content: {
         flexGrow: 1,
@@ -80,6 +29,7 @@ const useStyles = makeStyles((theme) => ({
 export default function Dashboard() {
     const classes = useStyles();
 
+    // Spots
     const [openSpots, setOpenSpots] = React.useState(false);
     const handleDrawerOpenSpots = () => {
         setOpenSpots(true);
@@ -91,34 +41,30 @@ export default function Dashboard() {
     return (
         <div className={classes.root}>
             <CssBaseline />
+            
+            {/* NAVIGATION */}
             <div style={{backgroundColor: '#2a6049', zIndex: '2000'}}>
-                <div className={classes.toolbar}>
-                    <IconButton onClick={openSpots ? handleDrawerCloseSpots : handleDrawerOpenSpots}>
-                        {openSpots ? <ChevronLeftIcon/> : <ChevronRightIcon/>}
-                    </IconButton>
-                </div>
                 <List button>
                     <ListItem>
                         <ListItemIcon>
-                            <ExploreIcon id="explore-icon" onClick={!openSpots ? handleDrawerOpenSpots : null}/>
+                            <ExploreIcon id="explore-icon" onClick={!openSpots ? handleDrawerOpenSpots : handleDrawerCloseSpots}/>
                         </ListItemIcon>
                     </ListItem>
                     <ListItem>
                         <ListItemIcon>
-                            <FavoriteIcon id="favorite-icon" onClick={!openSpots ? handleDrawerOpenSpots : null}/>
+                            <FavoriteIcon id="favorite-icon" />
                         </ListItemIcon>
                     </ListItem>
                 </List>
             </div>
-            <Drawer variant="permanent" 
-                    className={clsx(classes.drawer, {[classes.drawerOpen]: openSpots, [classes.drawerClose]: !openSpots})}
-                    classes={{paper: clsx({[classes.drawerOpen]: openSpots, [classes.drawerClose]: !openSpots})}}
-            >
-                <Spots/>
-            </Drawer>
+
+            {/* DRAWER FOR EVERY SECTION */}
+            <Spots open={openSpots}/>
+
+            {/* MAP */}
             <main className={classes.content}>
-                    <Map drawerOpened={openSpots.toString()}/>
-                </main>
+                <Map drawerOpened={openSpots.toString()}/>
+            </main>
         </div>
     );
 }
--- src/Spots/Spots.js
+++ src/Spots/Spots.js
@@ -1,8 +1,45 @@
 import React from 'react';
+import clsx from 'clsx';
+import Drawer from '@material-ui/core/Drawer';
+import { makeStyles } from '@material-ui/core/styles';
 
-export default function Spots() {
+const drawerWidth = '50vw';
+
+const useStyles = makeStyles((theme) => ({
+    drawer: {
+        width: drawerWidth,
+        flexShrink: 0,
+        whiteSpace: 'nowrap',
+    },
+    drawerOpen: {
+        width: drawerWidth,
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.enteringScreen,
+        }),
+    },
+    drawerClose: {
+        transition: theme.transitions.create('width', {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+        }),
+        overflowX: 'hidden',
+        width: theme.spacing(7) + 1,
+        [theme.breakpoints.up('sm')]: {
+            width: theme.spacing(9) + 1,
+        },
+    }
+}));
+
+export default function Spots(props) {
+    const classes = useStyles();
+    const { open } = props; 
     return(
-        <div style={{backgroundColor: 'yellow', width: '100vw', height: '100vh'}}>
-        </div>
+        <Drawer variant="permanent" 
+                className={clsx(classes.drawer, {[classes.drawerOpen]: open, [classes.drawerClose]: !open})}
+                classes={{paper: clsx({[classes.drawerOpen]: open, [classes.drawerClose]: !open})}}
+        >        
+            <div style={{backgroundColor: 'yellow', width: '100vw', height: '100vh'}}></div>
+        </Drawer>
     )
 }
