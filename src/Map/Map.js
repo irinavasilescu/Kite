aa3fca33e0d51b5e06e65c4d82bc9cf14337a676
Irina Vasilescu <irina>
2021-04-03 02:24:42 +0300

    fixed weird thingy with scroll when zooming on map

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -10,34 +10,20 @@ function Map(props) {
     }, [drawerOpened])
     
     return (
-        <div>
-            {/* 
-            NOT SURE I STILL WANT THIS HERE. I'LL THINK ABOUT IT.
-            { 
-                drawerOpened === 'true' ? 
-                <div style={{
-                    position: "absolute", 
-                    zIndex: 314159, 
-                    width: "100vw",
-                    height: "100vh", 
-                    background: "rgba( 255, 255, 255, 0.00 )",
-                    backdropFilter: "blur( 11.0px )"}}
-                >    
-                </div>
-                :
-                null
-            } */}
-            <MapContainer style={{height: '100vh'}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={true}>
-                <TileLayer
-                    attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.505, -0.09]}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
-                </Marker>
-            </MapContainer>
+        <div style={{border: "1px solid var(--ifm-color-emphasis-200)", position: "relative", padding: "1rem", borderBottomLeftRadius: "10px", borderBottomRightRadius: "10px"}}> 
+            <div>
+                <MapContainer style={{width: "100vw", height: "100vh"}} center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                    <Marker position={[51.505, -0.09]}>
+                        <Popup>
+                            A pretty CSS3 popup. <br /> Easily customizable.
+                        </Popup>
+                    </Marker>
+                </MapContainer>
+            </div>
         </div>
     );
 }
