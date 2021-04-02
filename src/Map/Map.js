4b3618ab5d430786ccd3aab74171ad197492392a
Irina Vasilescu <irina>
2021-04-02 16:13:25 +0300

    full-screen glass & removed shadow (looked weird on top of drawer)

--- src/Map/Map.js
+++ src/Map/Map.js
@@ -19,7 +19,6 @@ function Map(props) {
                     width: "100vw",
                     height: "100vh", 
                     background: "rgba( 255, 255, 255, 0.00 )",
-                    boxShadow: "0 8px 32px 0 rgba( 31, 38, 135, 0.37 )",
                     backdropFilter: "blur( 11.0px )"}}
                 >    
                 </div>
