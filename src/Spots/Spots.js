208278cf33cceed1021894b5a269d19083a788e0
Irina Vasilescu <irina>
2021-04-03 00:02:43 +0300

    displayed fetched data in the table

--- src/Spots/Spots.js
+++ src/Spots/Spots.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Table from '@material-ui/core/Table';
@@ -16,52 +16,36 @@ import './Spots.css';
 const drawerWidth = '100vw';
 
 const columns = [
-    { id: 'name', label: 'Name', minWidth: 170 },
-    { id: 'code', label: 'ISO\u00a0Code', minWidth: 100 },
-    {
-        id: 'population',
-        label: 'Population',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
+    { 
+        id: 'name', 
+        label: 'Name', 
+        minWidth: 100 
+    },
+    { 
+        id: 'country',
+        label: 'Country',
+        minWidth: 100 
     },
     {
-        id: 'size',
-        label: 'Size\u00a0(km\u00b2)',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toLocaleString('en-US'),
+        id: 'lat',
+        label: 'Latitude',
+        minWidth: 100
     },
     {
-        id: 'density',
-        label: 'Density',
-        minWidth: 170,
-        align: 'right',
-        format: (value) => value.toFixed(2),
+        id: 'long',
+        label: 'Longitude',
+        minWidth: 100
     },
-];
-
-function createData(name, code, population, size) {
-    const density = population / size;
-    return { name, code, population, size, density };
-}
-
-const rows = [
-    createData('India', 'IN', 1324171354, 3287263),
-    createData('China', 'CN', 1403500365, 9596961),
-    createData('Italy', 'IT', 60483973, 301340),
-    createData('United States', 'US', 327167434, 9833520),
-    createData('Canada', 'CA', 37602103, 9984670),
-    createData('Australia', 'AU', 25475400, 7692024),
-    createData('Germany', 'DE', 83019200, 357578),
-    createData('Ireland', 'IE', 4857000, 70273),
-    createData('Mexico', 'MX', 126577691, 1972550),
-    createData('Japan', 'JP', 126317000, 377973),
-    createData('France', 'FR', 67022000, 640679),
-    createData('United Kingdom', 'GB', 67545757, 242495),
-    createData('Russia', 'RU', 146793744, 17098246),
-    createData('Nigeria', 'NG', 200962417, 923768),
-    createData('Brazil', 'BR', 210147125, 8515767),
+    {
+        id: 'month',
+        label: 'Month',
+        minWidth: 100
+    },
+    {
+        id: 'probability',
+        label: 'Probability',
+        minWidth: 100
+    }
 ];
 
 const useStyles = makeStyles((theme) => ({
@@ -98,9 +82,10 @@ const useStyles = makeStyles((theme) => ({
 
 export default function Spots(props) {
     const classes = useStyles();
-    const [page, setPage] = React.useState(0);
-    const [rowsPerPage, setRowsPerPage] = React.useState(10);
     const { open } = props; 
+    const [ page, setPage ] = useState(0);
+    const [ rowsPerPage, setRowsPerPage ] = useState(10);
+    let [ spots, setSpots ] = useState([]);
 
     const handleChangePage = (event, newPage) => {
         setPage(newPage);
@@ -114,7 +99,10 @@ export default function Spots(props) {
     useEffect(() => {
         fetch('https://606640abb8fbbd0017568325.mockapi.io/spot')
             .then(response => response.json())
-            .then(data => console.log('DATA', data));
+            .then(data => {
+                console.log('DATA', data)
+                setSpots(data);
+            })
     }, []);
 
     return (
@@ -136,14 +124,14 @@ export default function Spots(props) {
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+                            {spots.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
                                 return (
-                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
+                                    <TableRow hover role="checkbox" tabIndex={-1} key={row.id}>
                                         {columns.map((column) => {
                                             const value = row[column.id];
                                             return (
                                             <TableCell key={column.id} align={column.align}>
-                                                {column.format && typeof value === 'number' ? column.format(value) : value}
+                                                {value}
                                             </TableCell>
                                             );
                                         })}
@@ -156,7 +144,7 @@ export default function Spots(props) {
                     <TablePagination
                         rowsPerPageOptions={[10, 25, 100]}
                         component="div"
-                        count={rows.length}
+                        count={spots.length}
                         rowsPerPage={rowsPerPage}
                         page={page}
                         onChangePage={handleChangePage}
